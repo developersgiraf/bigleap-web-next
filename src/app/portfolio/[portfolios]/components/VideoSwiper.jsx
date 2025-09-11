6d658a04b6b3e68d47a45ae4d5bd00b6b7d7a815
snehacmyk <115646372+snehacmyk@users.noreply.github.com>
2025-09-11 15:33:50 +0530

    Add video support to portfolio swiper
    Replaced image slides with autoplaying video slides in VideoSwiper, added biax.mp4 and seek.mp4 assets, and updated styles for improved layout and alignment. Swiper now uses Autoplay instead of Pagination, and navigation is disabled for a cleaner interface.

--- src/app/portfolio/[portfolios]/components/VideoSwiper.jsx
+++ src/app/portfolio/[portfolios]/components/VideoSwiper.jsx
@@ -3,68 +3,89 @@
 import React from "react";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
+import { Navigation, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
-import "swiper/css/pagination";
-import styles from "../../[portfolios]/portfolioDetail.module.scss";
+import styles from "./video.module.scss";
 
 export default function VideoSwiper({ styles }) {
   return (
     <Swiper
-      modules={[Navigation, Pagination]}
+      modules={[Navigation, Autoplay]}
       spaceBetween={30}
       slidesPerView={3}
       loop={true}
-      navigation={true}
-      pagination={{ clickable: true }}
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: true,
+      }}
+      navigation={false}
       className={styles.videoSwiperSlider}
     >
-      
-      
       <SwiperSlide>
         <div className={styles.videoSlide}>
-          <Image
-            src="/servicess/3danimation.png"
-            alt="3D Animation Project"
-            width={800}
-            height={400}
-            className={styles.slideImage}
+          <video
+            src="/portfolio/biax.mp4"
+            autoPlay={true}
+            muted={true}
+            loop={true}
+            controls={false}
+            playsInline={true}
+            preload="metadata"
+            width="100%"
+            height="400"
+            title="Animation Showreel"
           />
         </div>
       </SwiperSlide>
       
       <SwiperSlide>
         <div className={styles.videoSlide}>
-          <Image
-            src="/servicess/character.png"
-            alt="Character Design Project"
-            width={800}
-            height={400}
-            className={styles.slideImage}
+          <video
+            src="/portfolio/seek.mp4"
+            autoPlay={true}
+            muted={true}
+            loop={true}
+            controls={false}
+            playsInline={true}
+            preload="metadata"
+            width="100%"
+            height="400"
+            title="Animation Showreel"
           />
         </div>
       </SwiperSlide>
       
       <SwiperSlide>
         <div className={styles.videoSlide}>
-          <Image
-            src="/servicess/animation.png"
-            alt="Animation Project"
-            width={800}
-            height={400}
-            className={styles.slideImage}
+          <video
+            src="/portfolio/biax.mp4"
+            autoPlay={true}
+            muted={true}
+            loop={true}
+            controls={false}
+            playsInline={true}
+            preload="metadata"
+            width="100%"
+            height="400"
+            title="Animation Showreel"
           />
         </div>
       </SwiperSlide>
+      
       <SwiperSlide>
         <div className={styles.videoSlide}>
-          <Image
-            src="/servicess/character.png"
-            alt="Character Design Project"
-            width={800}
-            height={400}
-            className={styles.slideImage}
+          <video
+            src="/portfolio/seek.mp4"
+            autoPlay={true}
+            muted={true}
+            loop={true}
+            controls={false}
+            playsInline={true}
+            preload="metadata"
+            width="100%"
+            height="400"
+            title="Animation Showreel"
           />
         </div>
       </SwiperSlide>
