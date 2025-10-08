7f626cd775d0b5ba473b87118165d52ff7de61f4
saneshps <developersgiraf@gmail.com>
2025-10-08 15:27:05 +0530

    Adjust slider styles and breakpoints for responsiveness
    Commented out padding for .mySwiper in ImageSlider.module.css and updated top positioning in VideoSlider.module.css for medium desktop screens. Modified global padding for .abc.imageBtnSliderSection.mySwiper. Updated breakpoints in page.jsx to show more slides at 640px and adjusted spaceBetween values for improved layout on different screen sizes.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -114,7 +114,7 @@ export default function Home() {
               customSelector={"imageSliderSection abc"}
               breakpoints={{
                 320: { slidesPerView: 1, spaceBetween: 10 },
-                640: { slidesPerView: 2, spaceBetween: 0 },
+                640: { slidesPerView: 3, spaceBetween: 0 },
                 1024: { slidesPerView: 3, spaceBetween: 50 },
                 1366: { slidesPerView: 4, spaceBetween: 10 },
               }}
@@ -303,7 +303,7 @@ export default function Home() {
                 320: { slidesPerView: 1, spaceBetween: 10 },
                 640: { slidesPerView: 2, spaceBetween: 50 },
                 992: { slidesPerView: 2, spaceBetween: 40 },
-                1024: { slidesPerView: 2, spaceBetween: 20 },
+                1024: { slidesPerView: 2, spaceBetween: 30 },
                 1366: { slidesPerView: 3, spaceBetween: 0 },
               }}
               imageSize={400}
